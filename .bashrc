6addd92ef6618a804e4f92a3bde153b87635c9a9
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-08-07 20:20:51 -0700

    Easy rebase script

--- .bashrc
+++ .bashrc
@@ -2,8 +2,8 @@ alias updatea='sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y; sud
 
 alias hotspot='nmcli dev wifi hotspot ifname wlo1 ssid apeiron-garage password "replace_with_password"'
 
-alias git-pull-all='for i in */.git; do (cd $i/..; echo "** pulling ${i} ** branch: $(git rev-parse --abbrev-ref HEAD)"; git pull --recurse-submodules; echo ""; ); done'
-alias git_pull_all='git-pull-all'
+alias git_pull_all='for i in */.git; do (cd $i/..; echo "** pulling ${i} ** branch: $(git rev-parse --abbrev-ref HEAD)"; git pull --recurse-submodules; echo ""; ); done'
+alias rebase_from='bash ~/rebase_from.sh '
 
 alias pipenv_lock='time pipenv lock --pre --clear'
 alias pipenv_reinstall='pipenv uninstall --all; pipenv install --dev'
---
+++ rebase_from.sh
@@ -0,0 +1,40 @@
+# fail if there is no base branch specified
+if [ $# -eq 0 ]
+then
+	echo "Error, must specify base branch."
+	exit
+fi
+
+baseBranch=$1
+
+# check that base branch is different from current branch
+currentBranch=`git symbolic-ref --short HEAD`
+if [ $currentBranch = $baseBranch ]
+then
+	echo "Can't rebase from current branch, aborting."
+	exit
+fi
+
+# check that base branch exists
+git checkout $baseBranch
+if [ ! $? -eq 0 ]
+then
+	echo "Error pulling base branch, aborting."
+	exit
+fi
+
+# make sure that the base branch is up to date
+git pull
+# go back to the feature branch
+git checkout -
+# the -i option lets you rewrite commit messages
+git rebase -i $baseBranch
+git status
+
+# ask for confirmation to rewrite history on the remote
+read -p "Force push to remote? [y/N] " forcePush
+forcePush="${forcePush:-n}"
+if [ $forcePush = "y" ]
+then
+	git push --force
+fi
